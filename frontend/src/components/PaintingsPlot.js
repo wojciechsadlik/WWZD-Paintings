66ae17c8e18b648bd40f533a3377b100ed74fb93
wojciechsadlik <wojciech.sadlik.dev@gmail.com>
2022-01-24 10:35:07 +0100

    rebuilt database, added colors

--- frontend/src/components/PaintingsPlot.js
+++ frontend/src/components/PaintingsPlot.js
@@ -29,6 +29,11 @@ function getPaintingsPlotData(paintings) {
 }
 
 function PaintingsPlot(props) {
+  const colors = ["#3366cc", "#dc3912", "#ff9900", "#109618", "#990099",
+    "#0099c6", "#dd4477", "#66aa00", "#b82e2e", "#316395",
+    "#994499", "#22aa99", "#aaaa11", "#6633cc", "#e67300",
+    "#8b0707", "#651067", "#329262", "#5574a6", "#3b3eac"]
+
   const [paintings, setPaintings] = useState([]);
 
   useEffect(() => {
@@ -45,7 +50,7 @@ function PaintingsPlot(props) {
   } else {
     return (
       <div>
-        <Plot data={paintings} layout={{ width: 1280, height: 960 }} />
+        <Plot data={paintings} layout={{ width: 1280, height: 960, colorway: colors }}/>
       </div>
     );
   }
